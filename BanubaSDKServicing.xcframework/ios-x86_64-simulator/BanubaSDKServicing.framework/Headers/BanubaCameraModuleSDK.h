cdfd8985d68862461a7cd3201724c60911ffd501
Andrey Sak <andrei.sak@banuba.com>
2021-05-05 15:17:56 +0300

    1.0.15.3

---
+++ BanubaSDKServicing.xcframework/ios-x86_64-simulator/BanubaSDKServicing.framework/Headers/BanubaCameraModuleSDK.h
@@ -0,0 +1,18 @@
+//
+//  BanubaSDKServicing.h
+//  BanubaSDKServicing
+//
+//  Created by Gleb Markin on 2/25/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BanubaSDKServicing.
+FOUNDATION_EXPORT double BanubaSDKServicingVersionNumber;
+
+//! Project version string for BanubaSDKServicing.
+FOUNDATION_EXPORT const unsigned char BanubaSDKServicingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BanubaSDKServicing/PublicHeader.h>
+
+
